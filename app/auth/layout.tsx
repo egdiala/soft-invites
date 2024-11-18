4df8413ec88dbff5f50ab1fadbde98364cd4d170
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2024-11-18 20:32:53 +0100

    feat: add viewport

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,13 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "Soft Invites - Auth",
   description: "Authentication page for Soft Invites",
 };
+
+export const viewport: Viewport = {
+  themeColor: "#0D9488"
+}
 
 export default function RootLayout({
   children,
