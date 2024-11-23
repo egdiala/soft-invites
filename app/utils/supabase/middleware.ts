eaeb49dca357a74c375e2afe2e53d6ecde79360f
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2024-11-23 21:58:11 +0100

    fix: auth protected routes

--- app/utils/supabase/middleware.ts
+++ app/utils/supabase/middleware.ts
@@ -35,13 +35,17 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/auth')
-  ) {
+  if (!user && !request.nextUrl.pathname.startsWith('/auth')) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/auth/login'
+    return NextResponse.redirect(url)
+  }
+
+  if (user?.id && request.nextUrl.pathname.startsWith('/auth')) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone()
+    url.pathname = "/"
     return NextResponse.redirect(url)
   }
 
